b8daecfc5dadb3cd7fda5312223a735a48794a74
Cole Hille <colehille@gmail.com>
2020-01-22 01:29:56 -0500

    Adds working console solution, Dog.cs, and DogTest.cs

---
+++ ConsoleApp2/Dog.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2 {
+	public class Dog {
+
+		private int age;
+		private string name;
+
+		public Dog() {
+			age = 0;
+			name = "";
+		}
+
+		public Dog(int age, string name) {
+			this.setAge(age);
+			this.setName(name);
+		}
+
+		public int calcDogYears() {
+			int dogYears = 15 + (this.age - 1) * 7;
+			return dogYears;
+		}
+
+		public int getAge() {
+			return age;
+		}
+
+		public void setAge(int age) {
+			if (age < 0)
+				this.age = 0;
+			else
+				this.age = age;
+		}
+
+		public string getName() {
+			return name;
+		}
+
+		public void setName(string Name) {
+			bool valid = true;
+			char[] n = name.ToCharArray();
+
+			foreach (char c in n) {
+				if (!Char.IsLetter(c)) {
+					valid = false;
+					break;
+				}
+			}
+
+			if (valid)
+				this.name = name;
+			else
+				this.name = "";
+		}
+
+		public string toString() {
+			return ("Dog [age = " + age + ", name = " + name + "]");
+		}
+	}
+}
---
+++ ConsoleApp2/DogTest.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ConsoleApp2 {
+	class DogTest {
+		static void Main(string[] args) {
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
